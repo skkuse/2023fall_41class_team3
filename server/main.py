ac07f8d8595839820b1b33db15e26d36e1ac8bde
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2023-09-30 11:13:41 +0900

    Add API route: /api/runtime

--- server/main.py
+++ server/main.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+
+from .run import run_code
 
 app = Flask(__name__)
+
+
+@app.route("/api/runtime")
+def runtime():
+    code = request.get_json()
+    result, runtime = run_code(code)
+    return {"result": result, "runtime": runtime}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
