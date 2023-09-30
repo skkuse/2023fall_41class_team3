0dc3560ca08ead7ee371c2018d5f2cc6a3b93564
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2023-09-30 19:11:35 +0900

    Add Header styles

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <h1>Green Algorithms</h1>;
+  return (
+    <h1 className="text-center text-3xl my-10 tracking-widest">
+      GREEN ALGORITHMS
+    </h1>
+  );
 };
 
 export default Header;
