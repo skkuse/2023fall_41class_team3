c481eec6f68df3c63c65d7eb179e7961c9bdc1c9
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2023-09-30 19:11:48 +0900

    Add CodeInput styles

--- client/src/components/CodeInput/CodeInput.js
+++ client/src/components/CodeInput/CodeInput.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 const CodeInput = () => {
+  const [showCodeInput, setShowCodeInput] = useState(true);
   const [codeInput, setCodeInput] = useState("");
   const [codeResult, setCodeResult] = useState("");
   const [runtime, setRuntime] = useState(0);
 
   useEffect(() => {
     setCodeInput(localStorage.getItem("code") || "");
-  });
+  }, []);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -24,43 +25,118 @@ const CodeInput = () => {
       .then((data) => {
         setCodeResult(data.result);
         setRuntime(data.runtime);
-      });
+      })
+      .then(() => setShowCodeInput(false));
   };
 
   const handleTextareaChange = (e) => {
     setCodeInput(e.target.value);
   };
 
-  return (
-    <div style={{ display: "flex" }}>
-      <form method="POST" action="/api/runtime" onSubmit={handleFormSubmit}>
-        <h2>Code Input</h2>
-        <textarea
-          id="code-input"
-          name=""
-          cols="50"
-          rows="10"
-          value={codeInput}
-          onChange={handleTextareaChange}
-        />
-        <br />
+  const handleTextareaKeydown = (e) => {
+    if (e.key == "Tab") {
+      e.preventDefault();
+    }
+  };
 
-        <button type="submit">Submit</button>
-      </form>
-      <div>
-        <h2>Execution Results</h2>
-        <textarea
-          id="code-input"
-          name=""
-          cols="50"
-          rows="8"
-          value={codeResult}
-          onChange={() => {}}
-        />
-        <p>Runtime: {runtime}</p>
+  const handleButtomClick = () => {
+    setShowCodeInput(!showCodeInput);
+  };
+
+  const getEnergyNeeded = (runtime) => {
+    const power_draw = 1;
+    const PUE = 1.67;
+    const PSF = 1;
+    return runtime * power_draw * PUE * PSF;
+  };
+
+  const getCarbonFootprint = (runtime) => {
+    const energy_needed = getEnergyNeeded(runtime);
+    const carbon_intensity = 415.6;
+    return energy_needed * carbon_intensity;
+  };
+
+  const codeInputElement = (
+    <form method="POST" action="/api/runtime" onSubmit={handleFormSubmit}>
+      <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+        <h2 className="h-10 text-white flex items-center mx-5">
+          {showCodeInput ? "Code Input" : "Execution Results"}
+        </h2>
+
+        <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+          <label htmlFor="comment" className="sr-only">
+            Your comment
+          </label>
+          <textarea
+            id="code-input"
+            name=""
+            value={codeInput}
+            rows="4"
+            className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400 h-96 font-mono"
+            placeholder="Code Input"
+            onKeyDown={handleTextareaKeydown}
+            onChange={handleTextareaChange}
+            spellCheck="false"
+            required
+          />
+        </div>
+        <div className="flex justify-items-end flex-row-reverse px-3 py-2 border-t dark:border-gray-600">
+          <button
+            type="submit"
+            className="py-2.5 px-4 text-xs font-medium text-center text-white bg-green-700 rounded-lg focus:ring-4 focus:ring-green-200 dark:focus:ring-green-900 hover:bg-green-800"
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            className="mx-5 py-2.5 px-4 text-xs font-medium text-center text-white bg-green-700 rounded-lg focus:ring-4 focus:ring-green-200 dark:focus:ring-green-900 hover:bg-green-800"
+            onClick={handleButtomClick}
+          >
+            {showCodeInput ? "Execution Results" : "Code Input"}
+          </button>
+        </div>
       </div>
-    </div>
+    </form>
   );
+  const executionResultsElement = (
+    <form method="POST" action="/api/runtime" onSubmit={handleFormSubmit}>
+      <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+        <h2 className="h-10 text-white flex items-center mx-5">
+          {showCodeInput ? "Code Input" : "Execution Results"}
+        </h2>
+
+        <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+          <textarea
+            id="execution-results"
+            name=""
+            value={codeResult}
+            rows="4"
+            className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400 h-72 font-mono"
+            placeholder="Submit the code!"
+            spellCheck="false"
+            onChange={() => {}}
+            required
+          />
+        </div>
+        <ul className="h-16 text-white m-5">
+          <li>Runtime: {runtime} sec</li>
+          <li>Energy Needed: {getEnergyNeeded(runtime)} mg</li>
+          <li>Carbon Footprint: {getCarbonFootprint(runtime)}</li>
+        </ul>
+        <div className="flex justify-items-end flex-row-reverse px-3 py-2 border-t dark:border-gray-600">
+          <button
+            type="button"
+            className="mx-5 inline-flex py-2.5 px-4 text-xs font-medium text-center text-white bg-green-700 rounded-lg focus:ring-4 focus:ring-green-200 dark:focus:ring-green-900 hover:bg-green-800"
+            onClick={handleButtomClick}
+          >
+            {showCodeInput ? "Execution Results" : "Code Input"}
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+
+  return <>{showCodeInput ? codeInputElement : executionResultsElement}</>;
 };
 
 export default CodeInput;
