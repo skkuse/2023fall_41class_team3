fecb4cb49b0acd2c705ffa688a661a58241a0ec2
sonah5009 <sonah5009@gmail.com>
2023-11-02 16:50:59 +0900

    fix: page.jsx Padding 수정

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -5,21 +5,15 @@ import AlgoConst from "@/components/AlgoConst";
 import ServerEviornments from "@/components/ServerEviornments";
 import Runtime from "@/components/Runtime";
 import FootPrintResult from "@/components/FootPrintResult";
-import Banner from "@/components/Banner";
 
 const Home = () => {
   return (
-    <section className="flex-col w-full flex-center">
-      {/* <Banner /> */}
-      <h1 className="text-center head_text">
-        영상 배너 <br className="max-md:hidden" />
-        <span className="text-center green_gradient">CodEco</span>
-      </h1>
+    <section className="flex-col w-full">
       <div>
-        <div className="flex-center">
+        <div className="p-10 flex-center">
           <UserCode />
         </div>
-        <div className="flex flex-row gap-6 flex-center">
+        <div className="flex flex-row justify-center gap-6">
           <div>
             <AlgoConst />
             <ServerEviornments />
