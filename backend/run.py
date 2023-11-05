6d03ee21fcffc7fcdcf35aa2472a0d3280ebbaf2
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2023-11-05 20:42:46 +0900

    Add code for containerized code execution

--- backend/run.py
+++ backend/run.py
@@ -1,28 +1,7 @@
 import os
-import pathlib
 import subprocess
 from multiprocessing import Process
-
-
-def _run_container(code: str, pwd: str):
-    try:
-        container_run_info = subprocess.check_output(
-            f"docker run \
-                -it --rm \
-                -v {pwd}/container/exec_results.txt:/app/exec_results.txt\
-                ecoder:latest",
-            shell=True,
-        )
-        print(container_run_info.decode())
-
-    except Exception as e:
-        print(f"Docker container run failed: {e}")
-        return 0
-
-
-def _read_results() -> str:
-    # TODO
-    return ""
+from uuid import UUID
 
 
 def run_code(code: str, pwd: str):
@@ -36,3 +15,51 @@ def run_code(code: str, pwd: str):
 
     results = _read_results()
     return results
+
+
+def _copy_code(code: str, session_id: UUID, pwd: str) -> None:
+    """Copy user given code to a containerized directory
+
+    The user given code is written to a containerized directory. The neccesary
+    files required for execution is copied into a directory named by the
+    session id.
+
+    Args:
+        code (str): The user given code that should be written to the file
+        session_id (uuid.UUID): The session_id of the user
+        pwd (str): The path to where the main function is called
+
+    Returns:
+        None
+    """
+    path = os.path.join(pwd, "container", str(session_id))
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    with open(f"{pwd}/container/{str(session_id)}/Main.java", "x") as f:
+        f.write(code)
+
+    open(f"{pwd}/container/{str(session_id)}/execution_results.txt", "x")
+
+
+def _run_container(session_id: UUID, pwd: str):
+    container_path = os.path.join(pwd, "container", str(session_id))
+
+    try:
+        container_run_info = subprocess.check_output(
+            f"docker run \
+                -it --rm \
+                -v {container_path}:/app/\
+                ecoder:latest",
+            shell=True,
+        )
+        print(container_run_info.decode())
+
+    except Exception as e:
+        print(f"Docker container run failed: {e}")
+        return 0
+
+
+def _read_results() -> str:
+    # TODO
+    return ""
