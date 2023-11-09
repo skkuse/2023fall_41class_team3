cef1917e502a26421c434beabed68626bc31ab40
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2023-11-09 12:51:51 +0900

    Finalize runtime calculation logic

--- backend/run.py
+++ backend/run.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import subprocess
 from multiprocessing import Process
-from typing import Dict
+from typing import Dict, List
 from uuid import UUID
 
 
@@ -27,8 +27,16 @@ def run_code(code: str, session_id: UUID, pwd: str) -> Dict:
     p.start()
     p.join()
 
-    results = _read_results(session_id, pwd)
+    results = (
+        {"success": False}
+        if p.exitcode == 1
+        else {"success": True, **_read_results(session_id, pwd)}
+    )
+
     _clean_up(session_id, pwd)
+
+    if results["success"]:
+        pass
 
     return results
 
@@ -58,11 +66,28 @@ def _copy_code(code: str, session_id: UUID, pwd: str) -> None:
     open(f"{pwd}/container/{str(session_id)}/execution_results.txt", "x")
 
 
-def _run_container(session_id: UUID, pwd: str):
+def _run_container(session_id: UUID, pwd: str) -> None:
+    """Run the container
+
+    The directory named by the session_id will be connected to a docker container
+    via a bind mount. If the execution succeeds, the container will write the
+    execution results to the file 'execution_results.txt'.
+
+    Args:
+        session_id (uuid.UUID): The session_id of the user
+        pwd (str): The path to where the main function is called
+
+    Returns:
+        None
+
+    Exitcode:
+        1: The docker container failed to run.
+    """
+
     container_path = os.path.join(pwd, "container", str(session_id))
 
     try:
-        container_run_info = subprocess.check_output(
+        subprocess.check_output(
             f"docker run \
                 -it --rm \
                 -v {container_path}:/app/\
@@ -72,7 +97,7 @@ def _run_container(session_id: UUID, pwd: str):
 
     except Exception as e:
         print(f"Docker container run failed: {e}")
-        return 0
+        exit(1)
 
 
 def _read_results(session_id: UUID, pwd: str) -> Dict:
@@ -81,14 +106,46 @@ def _read_results(session_id: UUID, pwd: str) -> Dict:
     with open(os.path.join(container_path, "execution_results.txt"), "r") as f:
         lines = f.readlines()
 
-    code_output = "\n".join(lines[:-11])
-    runtime = lines[-11][5:]
+    code_output = "\n".join(lines[:-3])
+    runtime = parse_time(lines[-3:])
 
-    execution_result = {"code_output": code_output, "runtime": runtime}
-
-    return execution_result
+    return {
+        **runtime,
+        "code_output": code_output,
+    }
 
 
 def _clean_up(session_id: UUID, pwd: str) -> None:
     container_path = os.path.join(pwd, "container", str(session_id))
     shutil.rmtree(container_path)
+
+
+def parse_time(times: List[str]) -> Dict[str, float]:
+    """Parse time for execution results
+
+    The execution results are calculated using the bash function 'time'. The
+    results of this function are given as formatted strings. This function
+    transforms the output ot the time function to floating point numbers as
+    seconds.
+
+    Args:
+        times (List[str]): The result of the time function, should consist of
+        'real', 'user', 'sys'.
+
+    Returns:
+        The same list of values parsed into a dictionary of floating point numbers.
+    """
+    split_times = [
+        time.replace("m", " ").replace("s", " ").strip().split("\t")[1].split(" ")
+        for time in times
+    ]
+
+    result_times = []
+    for min, sec in split_times:
+        result_times.append(float(min) * 60 + float(sec))
+
+    return {
+        "runtime_real": result_times[0],
+        "runtime_user": result_times[1],
+        "runtime_sys": result_times[2],
+    }
