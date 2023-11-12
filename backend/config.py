b82b9db618e1443806c7ad8cf5000a27b0b03ffd
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2023-11-12 14:43:31 +0900

    Add database to backend

---
+++ backend/config.py
@@ -0,0 +1,18 @@
+import os
+
+import dotenv
+
+dotenv.load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ backend/db/models.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from db import db
+
+
+class CodeSubmission(db.Model):
+    submission_id = db.Column(db.String(100), primary_key=True)
+    submission_date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, submission_id):
+        self.submission_id = submission_id
+        self.submission_date = datetime.utcnow()
+
+    def to_json(self):
+        return {
+            "submission_id": self.submission_id,
+            "submission_date": self.submission_date.isoformat(),
+        }
+
+    def __repr__(self):
+        return f"<CodeSubmission {self.submission_id}>"
+
+
+class RefactoringStatistics(db.Model):
+    static_id = db.Column(db.Integer, primary_key=True)
+    refactoring_date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
+    reduction_amount = db.Column(db.Float, nullable=False)
+
+    def to_json(self):
+        return {
+            "static_id": self.static_id,
+            "refactoring_date": self.refactoring_date,
+            "reduction_amount": self.reduction_amount,
+        }
+
+    def __repr__(self):
+        return f"<RefactoringStatistics {self.static_id}>"
