36306ba5398a022c1702600c952d4d1d5b3c1597
lieion <50134433+lieion@users.noreply.github.com>
2023-11-15 21:03:46 +0900

    하단 탄소발자국 예시 추가

--- frontend/components/Results/FootPrintResult.jsx
+++ frontend/components/Results/FootPrintResult.jsx
@@ -4,6 +4,8 @@
 "use client";
 import React,{useState} from "react";
 import dynamic from 'next/dynamic'
+import Refactoring from "./Refactoring";
+import axios from "axios";
 
 //상단에 Result그림과 하단의 
 const ResultInfo = ({imgTitle, description, value, id, unit}) => {
@@ -13,8 +15,8 @@ const ResultInfo = ({imgTitle, description, value, id, unit}) => {
           <img style={{ margin: "0 auto" }} className="w-[60%] h-auto" src={imgTitle} alt="CarbonFootprintImage"/>
         </div>
         <div className="flex flex-col justify-start">
-          <div>{description}</div>
           <div id={id}> {value} {unit}</div>
+          <div>{description}</div>
         </div>
       </div>
   )
@@ -22,13 +24,26 @@ const ResultInfo = ({imgTitle, description, value, id, unit}) => {
 
 
 
+
 const FootPrintResult = ({resultsData}) => {
   const carbonFootprint= Number((resultsData?.carbon_footprint || 0.0).toFixed(2));
   const cpuUsage= Number((resultsData?.cpu_usage || 0.0).toFixed(2));
   const energyNeeded= Number((resultsData?.energy_needed || 0.0).toFixed(2));
+  const carbonFootprintDetailed = resultsData?.carbon_footprint || 0.0;
+  const tree_years = Math.floor(carbonFootprintDetailed / 11000);
+  const plane_percent = Number((((carbonFootprintDetailed / 171)/1147000)*100).toFixed(2));
+  const car_percent = Number((carbonFootprintDetailed / 175).toFixed(2));
+  const netflix_hour = Number((carbonFootprintDetailed / 36).toFixed(2));
+
   const [carbonFootprintImage, setCarbonFootprintImage] = useState("/assets/icons/carbon-footprint.svg"); //탄소 발자국 이미지
   const [energyNeededImage, setEnergyNeededImage] = useState("/assets/icons/energy.svg"); //에너지 필요량 이미지
-  const [cpuUsageImage, setCpuUsageImage] = useState("/assets/icons/co2_icon.svg"); //CPU 이미지로 변경 필요
+  const [cpuUsageImage, setCpuUsageImage] = useState("/assets/icons/co2_icon.svg");
+  const [treeImage, setTreeImage] = useState("/assets/icons/tree_icon.svg");
+  const [peopleImage, setPeopleImage] = useState("/assets/icons/people_icon.svg");
+  const [carImage, setCarImage] = useState("/assets/icons/car_icon.svg");
+  const [airplaneImage, setAirplaneImage] = useState("/assets/icons/airplane_icon.svg"); //CPU 이미지로 변경 필요
+
+
   return (
     <div className="flex flex-col justify-center items-start bg-surface p-2 gap-3">
       <div className="grid grid-cols-3 gap-3 w-full items-stretch justify-center items-center text-center bg-surface-dark p-2 rounded">   
@@ -56,34 +71,41 @@ const FootPrintResult = ({resultsData}) => {
       </div>
       <div className="grid grid-cols-2 gap-3 w-full items-stretch justify-center items-center text-center bg-surface-dark p-2 rounded">   
         <ResultInfo 
-          imgTitle={carbonFootprintImage}
-          description="Carbon Footprint" 
-          value={carbonFootprint} 
+          imgTitle={treeImage}
+          description="Carbon sequestration" 
+          value={tree_years} 
+          id="tree_years_text"
+          unit="tree-years"
+        />
+        <ResultInfo 
+          imgTitle={carImage}
+          description="in a passenger car" 
+          value={car_percent} 
           id="carbonFootprint_text"
-          unit="g CO2e"
+          unit="km"
         />
         <ResultInfo 
-          imgTitle={carbonFootprintImage}
-          description="Carbon Footprint" 
-          value={carbonFootprint} 
-          id="carbonFootprint_text"
-          unit="g CO2e"
+          imgTitle={airplaneImage}
+          description="of flight Seoul-Tokyo" 
+          value={plane_percent} 
+          id="flightSeoulTokyo_text"
+          unit="%"
         />
         <ResultInfo 
-          imgTitle={energyNeededImage}
-          description="Energy Needed" 
-          value={energyNeeded} 
-          id="energyNeeded_text"
-          unit="kWh"
-        />
-        <ResultInfo 
-          imgTitle={cpuUsageImage}
-          description="Cpu Usage" 
-          value={cpuUsage} 
-          id="cpuUsage_text"
-          unit=""
+          imgTitle={peopleImage}
+          description="streaming Netflix" 
+          value={netflix_hour} 
+          id="people_text"
+          unit="hours"
         />
       </div>
+      <div className="py-5 flex-center">
+          <button
+            onClick={startRefactoring}
+            className="px-16 py-2 text-white rounded-3xl bg-primary-green flex-center">
+            Refactoring
+          </button>
+        </div>
     </div>
   );
 };
