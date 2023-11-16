b17a824f22582dcc44db46aa993b4e660d4fbd2e
sonah5009 <sonah5009@gmail.com>
2023-11-16 19:15:51 +0900

    그래프 추가: AnalysisDiff.jsx 수정

--- frontend/components/Results/Refactoring/AnalysisDiff.jsx
+++ frontend/components/Results/Refactoring/AnalysisDiff.jsx
@@ -1,22 +1,76 @@
-// TODO: backend에서
-import React from "react";
-const FootPrintDiff = ({ fromValue, toValue }) => {
-  return <div></div>;
+// TODO: web page 띄울 때 뜨는 에러 해결할 것
+// Unhandled Runtime Error
+// Error: Canvas is already in use. Chart with ID '0' must be destroyed before the canvas with ID '' can be reused.
+// yarn add tw-elements
+"use client";
+
+import React, { useRef, useEffect } from "react";
+import { Chart, initTE } from "tw-elements";
+const MyChartComponent = () => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    initTE({ Chart });
+
+    const dataBarHorizontal = {
+      type: "bar",
+      data: {
+        labels: ["Original", "Suggested"],
+        datasets: [{ label: "Carbon footprint", data: [30, 15] }],
+      },
+    };
+
+    const optionsBarHorizontal = {
+      options: {
+        indexAxis: "y",
+        scales: {
+          x: {
+            stacked: true,
+            grid: {
+              display: true,
+              borderDash: [2],
+              zeroLineColor: "rgba(0,0,0,0)",
+              zeroLineBorderDash: [2],
+              zeroLineBorderDashOffset: [2],
+            },
+            ticks: {
+              color: "rgba(0,0,0, 0.5)",
+            },
+          },
+          y: {
+            stacked: true,
+            grid: {
+              display: false,
+            },
+            ticks: {
+              color: "rgba(0,0,0, 0.5)",
+            },
+          },
+        },
+      },
+    };
+    // 이전 차트 파기
+    if (chartRef.current) {
+      chartRef.current.clear;
+      chartRef.current.destroy;
+    }
+    new Chart(chartRef.current, dataBarHorizontal, optionsBarHorizontal);
+  }, []);
+
+  return <canvas ref={chartRef}></canvas>;
 };
-const EnergyNeedDiff = ({ fromValue, toValue }) => {
+const FootPrintDiff = ({ fromValue, toValue }) => {
   return <div></div>;
 };
 
 const AnalysisDiff = () => {
   return (
-    <div className="grid grid-cols-2 gap-2 p-3 rounded-xl bg-surface-dark">
-      <div className="flex-col bg-background flex-center rounded-xl">
-        <div>Carbon footprint</div>
-        <div>graph</div>
-      </div>
+    <div className="p-3 rounded-xl bg-surface-dark">
       <div className="flex-col flex-center bg-background rounded-xl">
         <div>Carbon footprint</div>
-        <div>graph</div>
+        <div class="mx-auto w-3/5 h-full overflow-hidden">
+          <MyChartComponent />
+        </div>
       </div>
     </div>
   );
