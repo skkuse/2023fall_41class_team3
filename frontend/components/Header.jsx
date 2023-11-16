ec08fc640b31512334f98e52879b7e8dfd977668
sonah5009 <sonah5009@gmail.com>
2023-11-16 14:27:46 +0900

    배너영상 크기 수정 및 영상 업로드

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -7,13 +7,13 @@ import { useState, useEffect } from "react";
 // import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 const Header = () => {
   return (
-    <div className="relative w-full items-center">
+    <div className="relative items-center w-full">
       {/* nav-bar 진녹색 투명 설정 */}
       <div
         id="nav-bar"
         className="z-20 top-0 w-full bg-primary-green/80 h-[10%] items-center absolute">
         <Link href="/">
-          <div className="flex content-center justify-center m-1 items-center">
+          <div className="flex items-center content-center justify-center m-1">
             <Image
               src="/assets/icons/logo.svg"
               alt="CodEco 로고"
@@ -30,7 +30,7 @@ const Header = () => {
       <div id="forest-player" className="top-0 w-full -z-10">
         <ReactPlayer
           className="top-0"
-          url="/assets/videos/banner.mp4"
+          url="/assets/videos/banner_pc.mp4"
           width="100%"
           height="100%"
           playing={true}
